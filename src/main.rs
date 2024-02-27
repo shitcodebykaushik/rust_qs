902028ed0e99a5451f913f53307cf04f02b1d29b
kaushik raj <algorithmunloack@gmail.com>
2024-02-27 23:23:02 +0530

    Keep coding high while thinking about system design

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::io;
 fn main(){
-    println!("Enter the new name ");
-    let mut name =String::new();
-    io::stdin ().read_line(&mut name).err();
-    println!("The new name is {}",name );
+  println!("Enter your name ");
+  let mut name = String::new();
+  io::stdin().read_line(&mut name).err();
+  println!("Your name is {}",name);
+  
 }
